628262bbc34a10bdfa586e8f6c97c0d34895c0fe
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-25 16:25:31 +0000

    changes 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,26 +1,44 @@
 #include "lists.h"
+#include <stdio.h>
+
 /**
- *check_cycle - check cycle
- *@list: list
- *Return: 0 or 1
- **/
+ * check_cycle - checks if singly linked list is a cycle
+ * @list: lista
+ * Return: 0 if no cycle, 1 is yes
+ */
 int check_cycle(listint_t *list)
 {
-	listint_t *aux = list;
-	listint_t *aux1 = list;
+	/*
+	 * Se crea dos variables para recorrer la lista "list" una itera de a 2
+	 * y la otra de a 1
+	 */
+	listint_t *faster = list;
+	listint_t *slow = list;
 
 	if (!list)
+	/*
+	 * Chequeamos si la lista que nos pasan existe y sino existe la lista se
+	 * retorna 0 y termina el prog
+	 */
 		return (0);
-	while (1)
+
+	while (1) /* Se crea un while infinito */
 	{
-		if (aux->next != NULL && aux1->next->next != NULL)
+		/* Traverse los nodos mientras que la linked list exista */
+		if (faster->next != NULL && faster->next->next != NULL)
 		{
-			aux = aux->next;
-			aux1 = aux1->next->next;
-			if (aux1 == aux)
+			faster = faster->next->next; /* el mas rapido itera de a 2 */
+			slow = slow->next; /* el mas lento itera de a 1 */
+
+			if (faster == slow)
+			/*
+			 * si en algun momento son iguales retornar 1 ya que
+			 * hay un ciclo
+			 */
 				return (1);
 		}
 		else
-			return (0);
+			return (0); /* Si no retornar 0 para salir del while infinito */
 	}
+
 }
