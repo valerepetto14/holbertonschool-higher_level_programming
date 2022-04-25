dacf3800a67214c45c93c5baeb28f178cbee533f
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-25 15:55:11 +0000

    changes in 10

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -13,14 +13,15 @@ int check_cycle(listint_t *list)
 		return (0);
 	while (aux1)
 	{
-
-		aux = aux->next;
-		aux1 = aux1->next->next;
 		if (aux && aux1)
 		{
-			if (aux == aux1)
-				return (1);
+			aux = aux->next;
+			aux1 = aux1->next->next;
+				if (aux == aux1)
+					return (1);
 		}
+		else
+			return (0);
 	}
 	return (0);
 }
