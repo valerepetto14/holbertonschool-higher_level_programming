d46b9ceff561d9c9776f7ecc5c78c8b40eb240a6
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-26 19:29:13 +0000

    add exercise 15

---
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -0,0 +1,9 @@
+#!/usr/bin/ls
+def remove_char_at(str, n):
+    str_tot = str
+    if (i == e):
+        continue
+    elif (i == 8 and e == 9):
+        print(f"{i}{e}", end='\n')
+    elif (i != e):
+        print(f"{i}{e}", end=", ")
