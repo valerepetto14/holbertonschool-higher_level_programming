ec908ed65854493572539aea30a9b3055d0ec410
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-28 16:26:51 +0000

    changes

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,22 +1,23 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-    from calculator_1 import add, sub, mul, div
     import sys
-    res = 0
-    if((len(sys.argv) - 1) != 3):
+    from calculator_1 import add, sub, mul, div
+    argc = len(sys.argv)
+    argv = sys.argv
+    if(argc - 1 != 3):
         print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
         exit(1)
     else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if(sys.argv[2] == "+"):
-            print(f"{a} + {b} = {add(a, b)}")
-        elif(sys.argv[2] == "-"):
-            print(f"{a} - {b} = {sub(a, b)}")
-        elif(sys.argv[2] == "*"):
-            print(f"{a} * {b} = {mult(a, b)}")
-        elif(sys.argv[2] == "/"):
-            print(f"{a} / {b} = {div(a, b)}")
+        a = int(argv[1])
+        b = int(argv[3])
+        if(argv[2] == "+"):
+            print(f"{a} {argv[2]} {b} = {add(a, b)}")
+        elif(argv[2] == "-"):
+            print(f"{a} {argv[2]} {b} = {sub(a, b)}")
+        elif(argv[2] == "*"):
+            print(f"{a} {argv[2]} {b} = {mul(a, b)}")
+        elif(argv[2] == "/"):
+            print(f"{a} {argv[2]} {b} = {div(a, b)}")
         else:
-            print(f"Unknown operator. Available operators: +, -, * and /")
+            print("Unknown operator. Available operators: +, -, * and /")
             exit(1)
