230f470d02bfed7b8dace0275d34b6e06819660c
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-28 15:06:17 +0000

    change in 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,5 +7,5 @@ if __name__ == "__main__":
         print(f"{len(sys.argv) - 1} argument:")
     else:
         print(f"{len(sys.argv) - 1} arguments:")
-     for iter in range(1, len(sys.argv)):
+    for iter in range(1, len(sys.argv)):
         print(f"{iter}: {sys.argv[iter]}")
