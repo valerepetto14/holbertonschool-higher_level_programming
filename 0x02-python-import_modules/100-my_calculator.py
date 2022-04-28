80b9cd2aa78fec52336b09fbfbb13b6091bc1e0c
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-28 16:07:13 +0000

    exercise 100

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,1 +1,20 @@
-iiiii
+#!/usr/bin/python3
+if __name__ == "__main__":
+    from calculator_1 import add, sub, mul, div
+    import sys
+    res = 0
+    if((len(sys.argv) - 1) != 3):
+        print(f"Usage: ./100-my_calculator.py <a> <operator> <b>")
+    else:
+        a = int(sys.argv[1])
+        b = int(sys.argv[3])
+        if(sys.argv[2] == "+"):
+            print(f"{a} + {b} = {add(a, b)}")
+        elif(sys.argv[2] == "-"):
+            print(f"{a} - {b} = {sub(a, b)}")
+        elif(sys.argv[2] == "*"):
+            print(f"{a} * {b} = {mult(a, b)}")
+        elif(sys.argv[2] == "/"):
+            print(f"{a} / {b} = {div(a, b)}")
+        else:
+            print(f"Unknown operator. Available operators: +, -, * and /")
