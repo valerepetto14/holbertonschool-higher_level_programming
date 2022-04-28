fb7335f24fa10f4ccdbffdacf51ca35a6ca74a88
valerepetto14 <valentinrepetto12345@gmail.com>
2022-04-28 15:05:29 +0000

    change in 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,10 +2,10 @@
 if __name__ == "__main__":
     import sys
     if((len(sys.argv) - 1) == 0):
-        print(f"{len(sys.argv) - 1} argument.")
+        print(f"{len(sys.argv) - 1} arguments.")
     elif ((len(sys.argv) - 1) == 1):
         print(f"{len(sys.argv) - 1} argument:")
     else:
         print(f"{len(sys.argv) - 1} arguments:")
-    for iter in range(1, len(sys.argv)):
+     for iter in range(1, len(sys.argv)):
         print(f"{iter}: {sys.argv[iter]}")
