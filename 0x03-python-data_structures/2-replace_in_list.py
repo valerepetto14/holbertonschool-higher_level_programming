d3deb06dc6a2d0caf751aade94669ad3a7595258
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 16:02:46 +0000

    changes border cases

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -3,7 +3,7 @@ def replace_in_list(my_list, idx, element):
     lenlist = len(my_list)
     if(idx < 0):
         return (my_list)
-    elif (idx > lenlist):
+    elif (idx >= lenlist):
         return (my_list)
     else:
         my_list[idx] = element
