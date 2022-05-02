bc9f3202925c1f4fe6a7f7aa1f35f2533cdba01e
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 19:03:09 +0000

    add exercise 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    new_list = []
+    for i in range(len(my_list)):
+        if my_list[i] % 2 == 0:
+            value = True
+            new_list.append(value)
+        else:
+            value = False
+            new_list.append(value)    
+    return (new_list)
