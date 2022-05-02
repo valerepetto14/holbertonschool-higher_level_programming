a542d65f6d30af4a4501f554a138dc257fa46219
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 18:40:29 +0000

    add exercise 9

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    if len(my_list) == 0:
+    if my_list:
         mayor = my_list[0]
         for i in range(len(my_list)):
             if(my_list[i] > mayor):
