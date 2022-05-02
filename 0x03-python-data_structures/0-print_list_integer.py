9fae06e603f59d70172738023c4b35e08a2285b8
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 15:38:21 +0000

    changes in exercise one

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
 def print_list_integer(my_list=[]):
-    for i in my_list:
-        print(f"{i}")
+    lista = [""]
+    tipo = type(lista)
+    if tipo == type(my_list):
+        for i in my_list:
+            print(f"{i}")
