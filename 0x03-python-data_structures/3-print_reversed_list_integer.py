9cc3dc2be2e7d2c5b7b47c861721c9b5c0d0cec2
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 16:11:37 +0000

    changes border cases

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     lentlist = len(my_list)
     lentlist -= 1
-    while (lentlist >= 0):
-        print(f"{my_list[lentlist]:d}")
-        lentlist -= 1
+    lista = []
+    if type(lista) == type(my_list):
+        while (lentlist >= 0):
+            print(f"{my_list[lentlist]:d}")
+            lentlist -= 1
