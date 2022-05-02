4f1cb3fb3d44776df1b867da32bf7674c6b87b2b
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 19:19:09 +0000

    add exercise 12

--- 0x03-python-data_structures/12-switch.py
+++ 0x03-python-data_structures/12-switch.py
@@ -1,12 +1,7 @@
-#!/usr/bin/python3
 # Edit this file.
 #!/usr/bin/python3
-
 def switch():
     a = 89
     b = 10
-    
-    a, b = 10, 89
-
-
+    a, b = 89, 10
     print("a={:d} - b={:d}".format(a, b))
