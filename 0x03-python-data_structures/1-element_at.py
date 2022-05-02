fd7454dd93be76361ca04fb04a080e4621735321
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 15:47:43 +0000

    changes border cases

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,9 +1,12 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
+    lista = []
     lenlist = len(my_list)
-    if(idx < 0):
-        return (NULL)
+    if type(lista) == type(my_list):
+        return None
+    elif(idx < 0):
+        return None
     elif (idx > lenlist):
-        return (NULL)
+        return None
     else:
         return (my_list[idx])
