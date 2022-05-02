5b37bac32ee3cb43fbd0a3544fe126e32926155f
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 16:20:14 +0000

    changes

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,7 +3,7 @@ def new_in_list(my_list, idx, element):
     lenlist = len(my_list)
     if(idx < 0):
         return (my_list)
-    elif (idx > lenlist):
+    elif (idx >= lenlist):
         return (my_list)
     else:
         new_list = my_list.copy()
