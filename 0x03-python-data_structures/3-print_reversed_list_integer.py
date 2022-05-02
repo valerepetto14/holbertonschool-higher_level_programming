73b4fd2a4fd57490ff1895d29f383f9d0875a29b
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 16:19:07 +0000

    changes

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,5 +3,5 @@ def print_reversed_list_integer(my_list=[]):
     lista = []
     if (type(lista) == type(my_list)):
         my_list.reverse()
-        for i in my_list
+        for i in my_list:
             print(f"{i:d}")
