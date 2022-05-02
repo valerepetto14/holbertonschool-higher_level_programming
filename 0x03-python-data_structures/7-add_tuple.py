587300aed69988728d49e4427f5241292a861335
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 17:33:51 +0000

    exercise 7

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    lenA = len(tuple_a)
+    lenB = len(tuple_b)
+    iter = 0
+    if(lenA < 2):
+        while lenA < 2:
+            tuple_a = tuple_a + (0,)
+            lenA += 1 
+    
+
+    iter = 0
+    if (lenB < 2):
+        while lenB < 2:
+             tuple_b = tuple_b + (0,)
+             lenB += 1
+
+    new_tuple = ((tuple_a[0] + tuple_b[0]),(tuple_a[1] + tuple_b[1]))
+    return (new_tuple)
