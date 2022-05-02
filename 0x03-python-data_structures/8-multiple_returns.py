c3074838880ff95a718d21477c86c3dde681c2a0
valerepetto14 <valerepetto12345@gmail.com>
2022-05-02 18:08:52 +0000

    execise 8

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if(sentence == ""):
+    if(!sentence):
         return None
     new_tuple = (len(sentence), sentence[0])
     return (new_tuple)
