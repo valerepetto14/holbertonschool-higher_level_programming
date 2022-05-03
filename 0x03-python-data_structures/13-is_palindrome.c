6cdbacccbf838489470d87c8cc11c3ff315988b7
valerepetto14 <valentinrepetto12345@gmail.com>
2022-05-03 01:46:19 +0000

    new

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,67 +1,39 @@
 #include "lists.h"
 /**
-*is_palindrome - verifica si es palindrome
-*@head: lista
-*Return: int
-**/
+* is_palindrome - checks if a singly linked list is a palindrome.
+* @head: header of a linked list
+* Return: 1 if it is a palindrome 0 otherwise
+*/
 int is_palindrome(listint_t **head)
 {
-	int iter = 0, len = 0, ultimo;
-	listint_t *aux = *head, *auxpar = *head, *recorrer = *head;
+	int *copy_list = NULL;
+	listint_t *aux = *head, *aux2 = *head;
+	int len = 0;
+	int i = 0;
 
-	if (head == NULL) /* non-existing list is not */
-		return (0);
-	if (!*head || (*head)->next == NULL)
-		return (1);
-	while (aux->next != NULL)
+	if (aux && aux->next != NULL)
 	{
-		aux = aux->next;
-		len++; }
-	len++;
-	ultimo = len;
-	if (len % 2 == 0)
-	{
-		while (iter < len / 2)
+		while (aux)
 		{
-			if (auxpar->n == indice_list(recorrer, ultimo))
-			{
-				ultimo = ultimo - 1;
-				auxpar = auxpar->next;
-				iter++; }
-			else
-				return (0); }
-	}
-	else
-	{
-		while (iter < (len / 2) - 1)
+			len++;
+			aux = aux->next;
+		}
+
+		copy_list = malloc(len * sizeof(int));
+		while (aux2)
 		{
-			if (auxpar->n == indice_list(recorrer, ultimo))
-			{
-				ultimo = ultimo - 1;
-				auxpar = auxpar->next;
-				iter++;
-			}
-			else
-				return (0); }
-	} return (1);
-}
+			copy_list[i] = aux2->n;
+			i++;
+			aux2 = aux2->next;
+		}
+		len--;
 
-/**
- *indice_list - printea
- *@head: pointer
- *@indice: int
- *Return: int
- **/
-int indice_list(listint_t *head, int indice)
-{
-	listint_t *aux = head;
-	int iter = 0;
-
-	indice--;
-	while (iter < indice && aux->next != NULL)
-	{
-		aux = aux->next;
-		iter++;
+		for (i = 0; i < len; i++)
+		{
+			if (copy_list[i] != copy_list[len])
+				return (0);
+			len--;
+		}
 	}
-	return (aux->n);
+	return (1);
 }
