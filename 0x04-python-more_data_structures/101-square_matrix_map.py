78ab8f0846e95dc9bf3c8265bab8d0ac5a872a90
valerepetto14 <valerepetto12345@gmail.com>
2022-05-04 18:45:22 +0000

    changes in sintaxis

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,3 +1,3 @@
 #!/usr/bin/python3
 def square_matrix_map(matrix=[]):
-    return list(map(lambda x: list(map(lambda x: x * x), x), matrix))
+    return list(map(lambda x: list(map((lambda a: a * a), x)), matrix))
