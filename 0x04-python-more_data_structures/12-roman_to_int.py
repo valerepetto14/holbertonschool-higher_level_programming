f92ab87c5b1da0747e21fa697e454adc5a0f21e8
valerepetto14 <valerepetto12345@gmail.com>
2022-05-04 19:48:49 +0000

    something errors

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
     signos = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
+    key = signos.keys()
     res = 0
+    iterador = 0
+    for i in range (len(roman_string)):
+        if i == 0:
+            res += signos[roman_string[i]]
+        else:
+            if 
+            return res
