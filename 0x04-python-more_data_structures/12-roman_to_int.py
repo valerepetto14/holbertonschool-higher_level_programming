27945b32b542cb8eb4b2bb4f8188dfa83cb5b427
valerepetto14 <valentinrepetto12345@gmail.com>
2022-05-05 01:05:47 +0000

    add exercise 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,12 +1,14 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
-    signos = {'I':1, 'V':5, 'X':10, 'L':50, 'C':100, 'D':500, 'M':1000}
-    key = signos.keys()
-    res = 0
-    iterador = 0
-    for i in range (len(roman_string)):
-        if i == 0:
-            res += signos[roman_string[i]]
+    if type(roman_string) is not str or roman_string is None:
+        return 0
+    if roman_string == "":
+        return 0
+    suma = 0
+    dic_val = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
+    for i in range(len(roman_string)):
+        if i > 0 and dic_val[roman_string[i]] > dic_val[roman_string[i - 1]]:
+            suma += dic_val[roman_string[i]] - 2 * dic_val[roman_string[i - 1]]
         else:
-            if 
-            return res
+            suma += dic_val[roman_string[i]]
+    return suma
