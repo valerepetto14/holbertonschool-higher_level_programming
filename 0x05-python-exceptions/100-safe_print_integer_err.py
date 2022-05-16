4dae0e3634383c92e45e59132181f161745e2fca
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-16 15:16:30 +0000

    fixed 100

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except Exception as error:
-        sys.stderr.write("{}\n".format(error))
+        sys.stderr.write("Exception: {}\n".format(error))
         return False
