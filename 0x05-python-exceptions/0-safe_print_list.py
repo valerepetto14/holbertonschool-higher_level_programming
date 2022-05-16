8ce917e062ea32dc0f5cfb95332384e2f85bac64
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-16 15:11:15 +0000

    fixed pycodestyle

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(f"{my_list[i]}", end="")
             count += 1
-        except(ValueError,IndexError):
+        except(ValueError, IndexError):
             print("")
             return(count)
     print("")
