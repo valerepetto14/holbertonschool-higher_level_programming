95c290084002892514d4941e3495a2a8dc2f00e4
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-16 16:15:12 +0000

    fixed 102

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,14 +1,13 @@
 #!/usr/bin/python
 def magic_calculation(a, b):
     result = 0
-    for i in range(1,3):
+    for i in range(1, 3):
         try:
-           if i > a:
-               raise Exception("too far")
-           else:
-               result += a**b/i
-        except:
+            if i > a:
+                raise Exception("too far")
+            else:
+                result += a**b/i
+        except Exception:
             result = a + b
             break
     return result
-
