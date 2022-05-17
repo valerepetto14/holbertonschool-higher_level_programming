30bc862494a5daf51a93f4e6a67f7ca3aba7a64a
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-17 18:58:48 +0000

    fixed 4

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,22 +1,22 @@
 #!/usr/bin/python3
 """
-define class square and constructor with their variables and methods setter and getter
+define class square and
+constructor with their variables and methods setter and getter
 """
 
 class Square:
-    '''define class square'''
     def __init__(self, size=0):
-        '''define constructor'''
+        """define constructor"""
         self.__size = size
 
     @property
     def size(self):
-        '''define method getter of size'''
+        """define method getter of size"""
         return self.__size
 
     @size.setter
     def size(self, value):
-        '''define method setter of size'''
+        """define method setter of size"""
         if type(value) != int:
             raise TypeError("size must be an integer")
         elif value < 0:
@@ -25,5 +25,5 @@ class Square:
             self.__size = value
 
     def area(self):
-        '''define method that calc the area of square'''
+        """define method that calc the area of square"""
         return self.size * self.size
