46639de3b7642ce401ccb2bdf139b644e72f68d9
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-18 22:20:09 +0000

    fixed 1

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -8,5 +8,35 @@ class Rectangle:
     """creamos la clase"""
     def __init__(self, width=0, height=0):
         """creamos el constructor"""
-        self.__heigth = heigth
-        self.__width = widht
+        self.heigth = heigth
+        self.width = width
+
+    @property
+    def width(self):
+        '''private instance attribute named width'''
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        '''setter of the private instance width'''
+        if type(value) is not int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        '''private instance attribute named height'''
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        '''setter of the private instance height'''
+        if type(value) is not int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
