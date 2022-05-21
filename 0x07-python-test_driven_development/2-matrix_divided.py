2124111a17a12acf838b6d71ec7430e28ca1c2fa
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-21 18:42:14 +0000

    add 2 with a one falls

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -6,23 +6,21 @@ function matrix devided
 def matrix_divided(matrix, div):
     """def matrix"""
     result = []
-    #if(matrix is None):
-        #raise TypeError("TypeError: matrix_divided() missing 1 required positional argument: 'div'")
-    #if (type(matrix) is not list and type(div) is not int):
-        #raise ValueError("matrix or div is not a possible value")
-    if matrix is None:
-        raise TypeError("matrix_divided() missing 1 required positional argument: 'div'")
-    if (type(matrix) is int):
-        raise TypeError("'int' object is not iterable")
-    if type(div) is not int and type(div) is not float:
-        raise TypeError("div must be a number")
     if div == 0:
         raise ZeroDivisionError("division by zero")
+    if matrix is None:
+        raise TypeError("matrix_divided() missing 1 required positional argument: 'div'")
+    if(matrix == []):
+        raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    if type(div) is not int and type(div) is not float:
+        raise TypeError("div must be a number")
     for i in matrix:
-        for a in i:
-            if(type(a) is not int and type(a) is not float):
-                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")   
-    
+        if(len(i) == 0):
+            raise TypeError("matrix must be a matrix (list of lists) of integers/floats")
+    for i in matrix:##recorro para saber si no tienen enteros o floats
+        for a in range(len(i)):
+            if(type(i[a]) is not int and type(i[a]) is not float):
+                raise TypeError("matrix must be a matrix (list of lists) of integers/floats")        
     for e in range(len(matrix)):
         if e == len(matrix) - 1:
             continue;
