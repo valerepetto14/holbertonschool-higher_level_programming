3332e039d567a74bc379c66e44067703f7affac0
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-24 21:47:41 +0000

    add 101 fixed

--- 0x09-python-everything_is_object/101-locked_class.py
+++ 0x09-python-everything_is_object/101-locked_class.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
+"""
+def class
+"""
 class LockedClass:
+    """
+    def lockedclasss
+    """
     __slots__ = ["first_name"]
 
     def __init__(self):
+        """def init"""
         self.first_name = "John"
