00f0376388b1ddc869fedb5888698663e84840c7
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-26 01:00:59 +0000

    fixed 4

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -12,5 +12,5 @@ def print_square(size):
         raise ValueError("size must be >= 0")
     for i in range(size):
         for a in range(size):
-            print("#",end="")
+            print("#", end="")
         print("")
