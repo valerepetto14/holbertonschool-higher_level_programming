c530337070e21619111c163a04795a71f1337215
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-30 22:10:04 +0000

    remove import

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -5,9 +5,6 @@ returns True if the object is an instance of a class that inherited
 """
 
 
-from operator import truediv
-
-
 def inherits_from(obj, a_class):
     """inherit from"""
     if (type(obj) != a_class) and (issubclass(type(obj), a_class) is True):
