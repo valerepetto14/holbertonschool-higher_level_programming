748537437a5bfa381f2e413767169cc7d09c9956
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-31 22:03:35 +0000

    add changes in 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,18 +1,16 @@
 #!/usr/bin/python3
-"""
-Write a script that adds all arguments
-to a Python list, and then save them to a file:
-"""
-
+"""asd asd asd"""
 
 from sys import argv
+
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
-file = 'add_item.json'
-try:
-    object = load_from_json_file(file)
-except FileNotFoundError:
-    object = []
+filename = "add_item.json"
 
-save_to_json_file(object + argv[1:], file)
+try:
+    load = load_from_json_file(filename)
+except FileNotFoundError:
+    load = []
+
+save_to_json_file(load + argv[1:], filename)
