6240f38dff1c5b43f57a7d53183593b14d9be12d
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-31 22:01:34 +0000

    changes in 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,12 +6,10 @@ to a Python list, and then save them to a file:
 
 
 from sys import argv
-
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 file = 'add_item.json'
-
 try:
     object = load_from_json_file(file)
 except FileNotFoundError:
