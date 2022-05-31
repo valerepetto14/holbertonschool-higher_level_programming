2e0120f9dbfdf47e4195fe54cfdfc0a37dc34812
valerepett14 <valentinrepetto12345@gmail.com>
2022-05-31 22:00:51 +0000

    add space en comments in 7

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -4,6 +4,7 @@ Write a script that adds all arguments
 to a Python list, and then save them to a file:
 """
 
+
 from sys import argv
 
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
