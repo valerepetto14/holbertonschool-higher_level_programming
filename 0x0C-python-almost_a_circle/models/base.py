75db39309b9a74709df2e7e6c339751a4b814f19
valerepett14 <valentinrepetto12345@gmail.com>
2022-06-06 16:38:58 +0000

    fix 17

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -42,7 +42,7 @@ class Base:
 
     def from_json_string(json_string):
         """def method from_json_string"""
-        if json_string is None or len(json_string):
+        if json_string is None or len(json_string) == 0:
             lista = []
         return json.loads(json_string)
 
