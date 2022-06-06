501b1371d0732587031bc9bc0da121ddfcf71693
valerepett14 <valentinrepetto12345@gmail.com>
2022-06-06 16:48:44 +0000

    fix 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,7 +21,7 @@ class Base:
     def to_json_string(list_dictionaries):
         """def method to_json_String"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            return ([])
+            list_dictionaries = ([])
         else:
             return json.dumps(list_dictionaries)
 
