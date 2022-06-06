5769add916203952cbe5e4810949c73d3d83cf41
valerepett14 <valentinrepetto12345@gmail.com>
2022-06-06 16:52:54 +0000

    fix 15

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,9 +21,8 @@ class Base:
     def to_json_string(list_dictionaries):
         """def method to_json_String"""
         if list_dictionaries is None or len(list_dictionaries) == 0:
-            list_dictionaries = ([])
-        else:
-            return json.dumps(list_dictionaries)
+            list_dictionaries = []
+        return json.dumps(list_dictionaries)
 
     @classmethod
     def save_to_file(cls, list_objs):
