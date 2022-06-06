ec16be8e4014d0c12a87eb04a4d13c984766646f
valentin repetto <valentinrepetto12345@gmail.com>
2022-06-06 21:03:23 +0000

    add test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -32,3 +32,20 @@ class test_Base(unittest.TestCase):
         new_json_string = Base.to_json_string([new_dictionary])
         self.assertEqual(new_json_string, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
     
+    def test_base_class(self):
+        """def test for type"""
+        b1 = Base()
+        self.assertEqual(type(b1), Base)
+        self.assertEqual(issubclass(Rectangle, Base), True)
+        self.assertEqual(issubclass(Square, Base), True)
+        self.assertEqual(issubclass(Rectangle, Square), False)
+        self.assertEqual(issubclass(Square, Rectangle), True)
+    
+    def test_save_to_file(self):
+        """def test save to file"""
+        with self.assertRaises(AttributeError):
+            Base.save_to_string()
+    
+    
+
+    
