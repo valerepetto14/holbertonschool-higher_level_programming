79a54b0558ee4c935916be16c7b54e8cee291109
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-06-06 15:59:31 -0300

    add test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,17 +1,31 @@
 #!/usr/bin/python3
-from re import M
+"""
+TEST OF CLASS BASE
+"""
 import unittest
-from pyrsistent import b
-from models import base
-from models import rectangle
-base = base.Base
-rectangle = rectangle.Rectangle
-"""
-TEST OF CLASS
-"""
+from models.base import Base
+from models.rectangle import Rectangle
+from models.square import Square
 
 
-class TestStringMethods(unittest.TestCase):
-    """test print"""
-    def test_print(self):
-        self.assertEqual(base(1), self.id == 1)
+class TestBase(unittest.TestCase):
+    """def class TestBase"""
+
+    def Test_id(self):
+        """test id"""
+        def test_print(self):
+            self.assertEqual(Base(1), self.id == 1)
+            self.assertEqual(Base(1000), self.id == 1000)
+            self.assertEqual(Base(3), self.id == 3)
+
+    def Test_type(self):
+        self.assertEqual(str(type(Base)), "<class 'type'>")
+        self.assertEqual(str(type(Rectangle)), "<class 'type'>")
+        self.assertEqual(str(type(Square)), "<class 'type'>")
+    
+    def Test_to_json_string(self):
+        """test method to json string"""
+        new_dictionary =  {'x': 2, 'width': 10, 'id': 1, 'height': 7, 'y': 8}
+        new_json_string = Base.to_json_string([new_dictionary])
+        self.assertEqual(new_json_string, '[{"x": 2, "width": 10, "id": 1, "height": 7, "y": 8}]')
+    
