4f8e3a06e4b045b52762ad0ec2e9b27d7d15ecfa
valentin repetto <valentinrepetto12345@gmail.com>
2022-07-05 23:48:19 +0000

    fixed 19

--- 0x0D-SQL_introduction/102-top_city.sql
+++ 0x0D-SQL_introduction/102-top_city.sql
@@ -1,2 +1,2 @@
 -- Import in hbtn_0c_0 database this table dump: download (same as Temperatures #0) -- 
-SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 ORDER BY avg_temp DESC LIMIT 3;
+SELECT city, AVG(value) AS avg_temp FROM temperatures WHERE month = 7 or month = 8 GROUP BY city ORDER BY avg_temp DESC LIMIT 3;
