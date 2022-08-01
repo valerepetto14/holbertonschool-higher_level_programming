3ffa31871267016b4b352f2dfedc10b0d0e74c1e
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-01 15:26:10 +0000

    fix semistandar 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,14 +6,13 @@ if (args.length === 2) {
   console.log(1);
 } else {
   const num = parseInt(args[2]);
-  console.log(factorial(num))
+  console.log(factorial(num));
 }
 
-function factorial(x) {
-	if (x == 0) {
-			return 1;
-	}
-	else {
-			return x * factorial(x - 1);
-	}
+function factorial (x) {
+  if (x === 0) {
+    return 1;
+  } else {
+    return x * factorial(x - 1);
+  }
 }
