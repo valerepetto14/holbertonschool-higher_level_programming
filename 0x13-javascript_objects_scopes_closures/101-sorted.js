ff72242deb37a6a291bdb3239c7f3db0fe866b58
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-02 19:12:48 +0000

    add 101

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,3 +1,19 @@
 #!/usr/bin/node
 const { dict } = require('./101-data');
+const newdic = [];
+const dicreturn = {};
 
+for (const clave in dict) {
+  newdic.push(dict[clave]);
+}
+const dicUnique = new Set(newdic);
+
+for (const i of dicUnique) {
+  dicreturn[i] = [];
+}
+for (const i in dict) {
+  const valor = dict[i];
+  dicreturn[valor].push(i);
+}
+
+console.log(dicreturn);
