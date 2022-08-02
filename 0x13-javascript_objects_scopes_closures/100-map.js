f6bb8491a84c6976259465af36abeb38ec9b7ee9
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-02 17:10:37 +0000

    add 100

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,8 +1,7 @@
 #!/usr/bin/node
 const { list } = require('./100-data');
 
-const newlist = list.map(function (dato, indice) {
-  return dato * indice;
-});
 console.log(list);
-console.log(newlist);
+console.log(list.map(function (dato, indice) {
+    return dato * indice;
+  }));
