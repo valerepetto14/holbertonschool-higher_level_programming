9a546ef2ab3d5f1037ef2da3f1fed5f4f6712aad
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 17:56:50 +0000

    fix 102

--- 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
+++ 0x0F-python-object_relational_mapping/102-relationship_cities_states_list.py
@@ -15,7 +15,7 @@ if __name__ == "__main__":
                                    sys.argv[3]), pool_pre_ping=True)
     Session = sessionmaker(bind=engine)
     session = Session()
-    citis = session.query(State,City).filter(State.id == City.state_id).all()
+    citis = session.query(State.City).filter(State.id == City.state_id).all()
     for i in citis:
         print(f"{i.City} -> {i.State.name}")
     session.close()
