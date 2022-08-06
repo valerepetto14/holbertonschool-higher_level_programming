70676c4b6570f2b937ced3e90d7a50e7ee0e18dd
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 15:57:47 +0000

    fix 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -11,16 +11,13 @@ if __name__ == "__main__":
     password = argv[2]
     name = argv[3]
     argument_search = argv[4]
-    try:
-        db = MySQLdb.connect("localhost", username, password, name)
-        cursor = db.cursor()
-        cursor.execute(f"SELECT * FROM states \
-                        WHERE states.name ='{argument_search}'\
-                                            ORDER BY states.id ASC")
-        data = cursor.fetchall()
-        for row in data:
-            print(row)
-        cursor.close()
-        db.close()
-    except(Exception) as error:
-        print(error)
+    db = MySQLdb.connect("localhost", username, password, name)
+    cursor = db.cursor()
+    cursor.execute(f"SELECT * FROM states \
+                    WHERE states.name ='{argument_search}'\
+                                        ORDER BY states.id ASC")
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
