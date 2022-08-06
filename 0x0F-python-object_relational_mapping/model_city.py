7de1391710a89fa8b69e4bc26b2c84639b59992a
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 17:25:14 +0000

    fix 14

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -8,16 +8,19 @@ from sqlalchemy.orm import relationship
 
 
 class City(Base):
+    """def city"""
     __tablename__ = 'cities'
     id = Column('id', Integer, primary_key=True, nullable=False, unique=True)
     name = Column('name', String(128), nullable=False)
     state_id = Column('state_id', Integer, ForeignKey('states.id'),
                       nullable=False)
-    """def init"""
+
     def __init__(self, name, state_id):
+        """def method init"""
         self.name = name
         self.state_id
 
     def __str__(self):
+        """def city"""
         return f"{self.id}: {self.name}"
         # legible para el humano ,simple string
