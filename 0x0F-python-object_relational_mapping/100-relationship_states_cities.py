4a5776e57c68ddf345078c8106c6750fdf69abe6
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 17:50:40 +0000

    fix 100

--- 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -1,51 +1,25 @@
 #!/usr/bin/python3
 """
-Write a script that creates the State “California” with the City “San Francisco
-” from the database hbtn_0e_100_usa: (100-relationship_states_cities.py):
-- Your script should take 3 arguments: mysql username, mysql password and
-database name
-- You must use the module SQLAlchemy
-- Your script should connect to a MySQL server running on localhost at port3306
-- You must use the cities relationship for all State objects
-- Your code should not be executed when imported
-Info backref: https://programmerclick.com/article/82401238841/
+Start link class to table in database
 """
-import sys
-# importamos lo que creamos en el script model_state.py
-from relationship_state import Base, State
-from relationship_city import City  # Usamos relationship_city
-# Import SQLAlchemy
-from sqlalchemy import (create_engine)
-from sqlalchemy.orm import sessionmaker
 
 
 if __name__ == "__main__":
-    # Connect to MySQL server running on localhost at port 3306
+    import sys
+    from relationship_state import Base, State
+    from relationship_city import City
+    from sqlalchemy.orm import sessionmaker
+    from sqlalchemy import (create_engine)
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
                            .format(sys.argv[1], sys.argv[2],
                                    sys.argv[3]), pool_pre_ping=True)
-
-    # Creamos las tablas que se importan en el script, en este caso 'states'
+    Session = sessionmaker(bind=engine)
+    session = Session()
     Base.metadata.create_all(engine)
-    # Uso la funcion Session para abrir la sesion
-    Session = sessionmaker(bind=engine)  # se une la sesion con el engine
-    session = Session()  # creamos una instancia de "Session"
-
-    # Create object State to add to hbtn_0e_100_usa
-    California = State(name="California")
-    # Create object city to add to hbtn_0e_100_usa
-    SanFrancisco = City(name="San Francisco")
-
-    # Se agrega en el estado california la ciudad San Francisco usando la
-    # relationship "city"
-    California.cities.append(SanFrancisco)
-
-    # Add objects to each table
-    session.add(California)
-    session.add(SanFrancisco)
-
-    # Save changes
+    newstate = State('California')
+    newcity = City('San Francisco')
+    newstate.cities.append(newcity)
+    session.add(newstate)
+    session.add(newcity)
     session.commit()
-
-    # Close session that was opened
     session.close()
