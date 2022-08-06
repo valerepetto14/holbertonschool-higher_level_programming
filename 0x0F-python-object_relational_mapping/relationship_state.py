d3d7b652b8b82243c18770bb4409b99c7b2b0998
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 17:31:26 +0000

    add comennts

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,15 +5,17 @@ Base = declarative_base()
 
 
 class State(Base):
+    """def state"""
     __tablename__ = 'states'
     id = Column('id', Integer, primary_key=True, nullable=False, unique=True)
     name = Column('name', String(128), nullable=False)
     cities = relationship('City', backref='state',cascade='all, delete-orphan')
 
-    """def init"""
     def __init__(self, name):
+        """def init"""
         self.name = name
 
     def __str__(self):
+        """def str"""
         return f"{self.id}: {self.name}"
         # legible para el humano ,simple string
