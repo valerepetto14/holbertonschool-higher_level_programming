4efd80fd6b0aebcd62e019df2532b21aec1ef882
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 16:14:40 +0000

    add changes in 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,22 +5,18 @@ sql alchemy
 
 
 if __name__ == "__main__":
-    try:
-        from sys import argv
-        import MySQLdb
-        username = argv[1]
-        password = argv[2]
-        name = argv[3]
-        argument_search = argv[4]
-        db = MySQLdb.connect("localhost", username, password, name)
-        cursor = db.cursor()
-        cursor.execute(f"SELECT * FROM states \
-                        WHERE states.name LIKE BINARY '{argument_search}'\
-                                            ORDER BY states.id ASC")
-        data = cursor.fetchall()
-        for row in data:
-            print(row)
-        cursor.close()
-        db.close()
-    except (Exception) as errors:
-        print(errors)
+    from sys import argv
+    import MySQLdb
+    username = argv[1]
+    password = argv[2]
+    name = argv[3]
+    argument_search = argv[4]
+    db = MySQLdb.connect("localhost", username, password, name)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE BINARY '{}'\
+          ORDER BY id ASC".format(argument_search))
+    data = cursor.fetchall()
+    for row in data:
+        print(row)
+    cursor.close()
+    db.close()
