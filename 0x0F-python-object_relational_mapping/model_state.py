0190b8d202195335f7279d2b236cc2ea8f5a6205
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 16:23:44 +0000

    add changes in model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -15,7 +15,6 @@ class State(Base):
     id = Column('id', Integer, primary_key=True, nullable=False, unique=True)
     name = Column('name', String(128), nullable=False)
     citis = relationship('City', backref='father')
-
     """def init"""
     def __init__(self, name):
         self.name = name
