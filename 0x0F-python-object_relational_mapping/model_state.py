c9e0509343024807d083f6ec4d65670458d30e14
Valentin Repetto <valentinrepetto12345@gmail.com>
2022-08-06 16:26:45 +0000

    fix model state

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -11,14 +11,17 @@ Base = declarative_base()
 
 
 class State(Base):
+    """model state"""
     __tablename__ = 'states'
     id = Column('id', Integer, primary_key=True, nullable=False, unique=True)
     name = Column('name', String(128), nullable=False)
     citis = relationship('City', backref='father')
 
     def __init__(self, name):
+        """method init"""
         self.name = name
 
     def __str__(self):
+        """methos str"""
         return f"{self.id}: {self.name}"
         # legible para el humano ,simple string
