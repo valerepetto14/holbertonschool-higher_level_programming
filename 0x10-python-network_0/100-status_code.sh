51fb8c23456f3a3f4f3cd740e23737b82ab4ec66
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-18 18:03:01 +0000

    fix 100

--- 0x10-python-network_0/100-status_code.sh
+++ 0x10-python-network_0/100-status_code.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that sends a request to a URL passed as an argument, and displays only the status code of the response.
-curl -LI "$1" -o /dev/null -w '%{http_code}\n' -s
+curl -s -o /dev/null -w "%{http_code}" "$1"
