40e9de848ec035baebbf9fce164a238cde739c7e
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-18 19:53:27 +0000

    add 101

--- 0x10-python-network_0/101-post_json.sh
+++ 0x10-python-network_0/101-post_json.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 #Write a Bash script that sends a JSON POST request to a URL passed as the first argument, and displays the body of the response.
-curl -X POST "$1" 'Content-Type: application/json'-d '{"name": "John Doe",  "age": 33}'
+curl -X POST "$1" -H 'Content-Type: application/json' -d @"$2"
