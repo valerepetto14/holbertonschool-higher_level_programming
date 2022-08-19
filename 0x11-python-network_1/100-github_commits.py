2a27f097ab98d5ef217d4fc1776b9398810bac5c
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-19 20:25:16 +0000

    fix 100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python3
-""" task 100 """
-
-from sys import argv
-from requests import get
-
+"""
+Write a Python script that takes 2 arguments
+in order to solve this challenge.
+"""
 
 if __name__ == "__main__":
-    url = "https://api.github.com/repos/{}/{}/commits".format(argv[2], argv[1])
-    r = get(url)
-    for comit in r.json()[0:10]:
-        print(comit.get('sha'), end=': ')
-        print(comit.get('commit').get('author').get('name'))
+    import requests
+    from sys import argv
+
+    repo = argv[1]
+    user = argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/commits'.format(user, repo)
+    res = requests.get(url)
+    data = res.json()
+    cont = 0
+    for i in data:
+        if cont < 10:
+            print(i.get('sha'), end=': ')
+            print(i.get('commit').get('author').get('name'))
+            cont = cont + 1
