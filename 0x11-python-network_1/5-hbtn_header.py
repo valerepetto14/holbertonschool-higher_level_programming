02581e075ba84bd839e5c8a9a1c0a1ce318ce64a
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-19 16:43:20 +0000

    fix 4

--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -4,7 +4,8 @@
 
 if __name__ == "__main__":
     import requests
+    import sys
 
-    res = requests.get("https://intranet.hbtn.io/status")
+    res = requests.get(sys.argv[1])
     print(res.headers['X-Request-Id'])
     
