5942276ac2433d1adc73b85e077757c7b72785b6
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-19 15:11:29 +0000

    fix 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,9 +3,6 @@
 import urllib.request
 
 
-import urllib.request
-
-
 if __name__ == "__main__":
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.headers.get('X-Request-Id')
