944e9d1d50c97ceef86d92848b29d77a505c31eb
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-19 17:58:13 +0000

    fix 7

--- 0x11-python-network_1/7-error_code.py
+++ 0x11-python-network_1/7-error_code.py
@@ -8,6 +8,6 @@ if __name__ == "__main__":
 
     res = requests.get(sys.argv[1])
     if (res.status_code >= 400):
-        print("Error code: ", res.status_code)
+        print("Error code:", res.status_code)
     else:
         print(res.text)
