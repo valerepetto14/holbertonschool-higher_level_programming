9c69e4add2f939869ae3ce2eebe3903200998f7e
valentin repetto <valentinrepetto12345@gmail.com>
2022-08-19 15:22:38 +0000

    fix 1

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,12 +1,10 @@
 #!/usr/bin/python3
 """Write a Python script that fetches https://intranet.hbtn.io/status"""
-import urllib.request
-
-
-import urllib.request
 
 
 if __name__ == "__main__":
+    import urllib.request
+
     with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
         content = response.headers.get('X-Request-Id')
         print(content)
