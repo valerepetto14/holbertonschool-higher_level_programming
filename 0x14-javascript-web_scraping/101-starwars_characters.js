77ed9f5d167ee3256e7bb8e891393d0b37e2c5b5
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-12 17:43:00 +0000

    fix 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,18 +2,35 @@
 // Write a script that prints all characters of a Star Wars movie:
 const axios = require('axios');
 const episodio = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/`;
+const url = `https://swapi-api.hbtn.io/api/films/${episodio}`;
 
-axios.get(url)
-  .then(response => {
-    const peliculas = response.data.results;
-    const actores = peliculas[episodio - 1].characters
-    for (let i = 0; i < actores.length; i++) {
-      axios.get(actores[i])
-        .then(actors => {
-          console.log(actors.data.name);
-        });
-   }})
-  .catch(error => {
-    console.log(error.response.status);
-  });
+// axios.get(url)
+//   .then(response => {
+//     const actores = response.data.characters;
+//     for (let i of actores) {
+//       axios.get(i)
+//         .then(actors => {
+//           console.log(actors.data.name);
+//         })
+//         .catch(err => {
+//             console.log(err)
+//         })
+//    }})
+//   .catch(error => {
+//     console.log(error.response.status);
+//   });
+
+async function actores () {
+  try {
+    const response = await axios.get(url);
+    const actores = response.data.characters;
+    for (const i of actores) {
+      const actor = await axios.get(i);
+      console.log(actor.data.name);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+actores();
