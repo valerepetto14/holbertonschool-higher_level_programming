b3393dd2a42a66b3bedc000c32cb2ba799f3da89
valentin repetto <valentinrepetto12345@gmail.com>
2022-09-12 17:13:16 +0000

    fix 101

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -2,18 +2,18 @@
 // Write a script that prints all characters of a Star Wars movie:
 const axios = require('axios');
 const episodio = process.argv[2];
-const url = `https://swapi-api.hbtn.io/api/films/${episodio}`;
+const url = `https://swapi-api.hbtn.io/api/films/`;
 
 axios.get(url)
   .then(response => {
-    const actores = response.data.characters;
+    const peliculas = response.data.results;
+    const actores = peliculas[episodio - 1].characters
     for (let i = 0; i < actores.length; i++) {
       axios.get(actores[i])
         .then(actors => {
           console.log(actors.data.name);
         });
-    }
-  })
+   }})
   .catch(error => {
     console.log(error.response.status);
   });
